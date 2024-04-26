fb6fa36eca4ded28202dd036b2d9b5801da92359
Bijay Subedi <bijay.subedi@vedastudios.com>
2024-04-26 10:59:52 +0545

    chore: add console.log

--- src/components/ExampleComponent.tsx
+++ src/components/ExampleComponent.tsx
@@ -58,6 +58,8 @@ const Group = [
   },
 ];
 
+console.log("Test Build");
+
 export const ExampleComponent = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
