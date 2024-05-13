3529f19727099c49ba164f0b28092a3b240d66cc
Bijay Subedi <bijay.subedi@vedastudios.com>
2024-05-13 15:29:59 +0545

    fix: enhancement on topbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,61 +30,73 @@ export default function RootLayout({
               {(storedData?.username || configuration?.username) && (
                 <>
                   <div className="flex flex-col w-full">
-                    <div className="flex gap-2">
-                      <span className="font-bold text-[14px]">Server : </span>
-                      <span className="font-bold text-[14px] text-blue-500 flex gap-2 items-center">
-                        {storedData?.serverBaseUrl && storedData?.serverBaseUrl}
-                        <CopyContent
-                          content={
-                            storedData?.serverBaseUrl
-                              ? storedData?.serverBaseUrl
-                              : ""
-                          }
-                        />
-                      </span>
-                    </div>
-
-                    <div className="flex gap-2">
-                      <span className="font-bold text-[14px]">
-                        Socket url :
-                      </span>
-                      <span className="font-bold text-[14px] text-blue-500 flex gap-2 items-center">
-                        {storedData?.socketUrl && storedData?.socketUrl}
-                        <CopyContent
-                          content={
-                            storedData?.socketUrl ? storedData?.socketUrl : ""
-                          }
-                        />
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center gap-6">
-                    <div className="flex flex-col w-[500px]">
+                    {storedData?.serverBaseUrl && (
                       <div className="flex gap-2">
-                        <span className="font-bold text-[14px]">
-                          Username :
-                        </span>
-                        <span className="font-bold text-[14px] text-blue-500">
-                          {storedData?.username
-                            ? storedData?.username
-                            : configuration?.username}
-                        </span>
-                      </div>
-                      <div className="flex gap-2">
-                        <span className="font-bold text-[14px]">UserId : </span>
-                        <span className="font-bold text-[14px] flex gap-2 items-center text-blue-500">
-                          {storedData?.userId
-                            ? storedData?.userId
-                            : configuration?.userId}
-
+                        <span className="font-bold text-[14px]">Server : </span>
+                        <span className="font-bold text-[14px] text-blue-500 flex gap-2 items-center">
+                          {storedData?.serverBaseUrl &&
+                            storedData?.serverBaseUrl}
                           <CopyContent
                             content={
-                              storedData?.userId || configuration?.userId
+                              storedData?.serverBaseUrl
+                                ? storedData?.serverBaseUrl
+                                : ""
                             }
                           />
                         </span>
                       </div>
+                    )}
+
+                    {storedData?.socketUrl && (
+                      <div className="flex gap-2">
+                        <span className="font-bold text-[14px]">
+                          Socket url :
+                        </span>
+                        <span className="font-bold text-[14px] text-blue-500 flex gap-2 items-center">
+                          {storedData?.socketUrl && storedData?.socketUrl}
+                          <CopyContent
+                            content={
+                              storedData?.socketUrl ? storedData?.socketUrl : ""
+                            }
+                          />
+                        </span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex items-center gap-6">
+                    <div className="flex flex-col w-[500px]">
+                      {(storedData?.username || configuration?.username) && (
+                        <div className="flex gap-2">
+                          <span className="font-bold text-[14px]">
+                            Username :
+                          </span>
+                          <span className="font-bold text-[14px] text-blue-500">
+                            {storedData?.username
+                              ? storedData?.username
+                              : configuration?.username}
+                          </span>
+                        </div>
+                      )}
+
+                      {(storedData?.userId || configuration?.userId) && (
+                        <div className="flex gap-2">
+                          <span className="font-bold text-[14px]">
+                            UserId :{" "}
+                          </span>
+                          <span className="font-bold text-[14px] flex gap-2 items-center text-blue-500">
+                            {storedData?.userId
+                              ? storedData?.userId
+                              : configuration?.userId}
+
+                            <CopyContent
+                              content={
+                                storedData?.userId || configuration?.userId
+                              }
+                            />
+                          </span>
+                        </div>
+                      )}
                     </div>
                     <div
                       onClick={() => {
