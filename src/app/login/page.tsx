17697fd31b9034f797a83eb0e7adb1bf410fccff
Bijay Subedi <bijay.subedi@vedastudios.com>
2024-05-27 15:49:09 +0545

    fix: handle the api not found issue in login, implement the actual login url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ const generateRandomWord = () => {
 
 const Login = () => {
   const [baseUrl, setBaseUrl] = useState(
-    "https://dev-api.marsenger.com/api/v1"
+    "https://dev-api.marsenger.com/api/v2/auth/login"
   );
   const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
@@ -56,7 +56,7 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const { data } = await axios.post(`${baseUrl}/auth/login`, {
+      const { data } = await axios.post(baseUrl, {
         username,
         password,
         registrationToken: `${randomWord}n1boH6po7f8OBYT1SGGQn:Awao1fEeJPOzzqd2oqg2tflnZ_e1uZF5p4AbbFrVHMOLb6Znh6Uhe_vYxDit41J3KFbzUKYybpLKiwFTEdSq-yRSqFbJsbsQNuV3kF1ACsKUd-lK_8RXFoyAeGCje2vg6D_QMJq3fm6i`,
@@ -84,10 +84,17 @@ const Login = () => {
       router.push("/");
     } catch (err: any) {
       console.log(err);
-      setResponseMessage({
-        message: err?.response?.data?.message,
-        type: "ERROR",
-      });
+      if (err.response && err.response.status === 404) {
+        setResponseMessage({
+          message: "404 - Api not found",
+          type: "ERROR",
+        });
+      } else {
+        setResponseMessage({
+          message: err?.response?.data?.message || "An error occurred",
+          type: "ERROR",
+        });
+      }
     } finally {
       setLoading(false);
     }
@@ -95,33 +102,43 @@ const Login = () => {
 
   return (
     <div className="w-full flex justify-center py-6 items-center">
-      <div className="w-full lg:w-[700px] pb-20 shadow-lg border rounded-md flex flex-col gap-6 py-5 px-10">
+      <div className="w-full lg:w-[800px] pb-20 shadow-lg border rounded-md flex flex-col gap-6 py-5 px-10">
         <div className="p-10 flex flex-col gap-6">
           <div className="flex flex-col gap-2">
             <div className="flex flex-row gap-2 items-center">
-              <span className="font-bold">Staging server :</span>
-              <span className="text-blue-500">
-                https://staging-api.marsenger.com/api/v1
+              <span className="text-[15px] font-bold">Staging server :</span>
+              <span className="text-blue-500 text-[14px]">
+                https://staging-api.marsenger.com/api/v2/auth/login
               </span>
-              <CopyContent content="https://staging-api.marsenger.com/api/v1" />
+              <CopyContent content="https://staging-api.marsenger.com/api/v2/auth/login" />
             </div>
             <div className="flex flex-row gap-2 items-center">
-              <span className="font-bold">Development server :</span>
-              <span className="text-blue-500">
-                https://dev-api.marsenger.com/api/v1
+              <span className="text-[15px] font-bold">
+                Development server :
               </span>
-              <CopyContent content="https://dev-api.marsenger.com/api/v1" />
+              <span className="text-blue-500 text-[14px]">
+                https://dev-api.marsenger.com/api/v2/auth/login
+              </span>
+              <CopyContent content="https://dev-api.marsenger.com/api/v2/auth/login" />
             </div>
             <div className="flex flex-row gap-2 items-center">
-              <span className="font-bold">Local server :</span>
-              <span className="text-blue-500">https://chat-app.dev/api/v1</span>
-              <CopyContent content="https://chat-app.dev/api/v1" />
+              <span className="text-[15px] font-bold">Local server :</span>
+              <span className="text-blue-500 text-[14px]">
+                https://chat-app.dev/api/v2/auth/login
+              </span>
+              <CopyContent content="https://chat-app.dev/api/v2/auth/login" />
             </div>
           </div>
           <h1 className="font-bold text-xl">Login Window</h1>
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <label>Server Base Url</label>
+            <div className="flex flex-col gap-2 text-sm">
+              <label className="w-full">
+                Login Url (
+                <span className="text-blue-500 text-sm font-bold">
+                  Please specify the login URL for requesting access.
+                </span>
+                )
+              </label>
               <input
                 value={baseUrl}
                 autoComplete="on"
@@ -130,11 +147,11 @@ const Login = () => {
                 onChange={(e) => {
                   setBaseUrl(e.target.value);
                 }}
-                placeholder="Username"
+                placeholder="Please specify the login url"
               />
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 text-sm">
               <label>Username</label>
               <input
                 autoComplete="on"
@@ -146,7 +163,7 @@ const Login = () => {
                 placeholder="Username"
               />
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 text-sm">
               <label>Password</label>
               <input
                 autoComplete="on"
@@ -174,7 +191,7 @@ const Login = () => {
             <button
               disabled={!username && !password && loading}
               type="submit"
-              className={`p-2 bg-gray-700 ${
+              className={`p-2 text-sm bg-gray-700 ${
                 !username && !password && loading
                   ? "!bg-blue-300 !cursor-not-allowed"
                   : "!bg-blue-600 !cursor-pointer"
