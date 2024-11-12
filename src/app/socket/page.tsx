423456e3486e4d2c04cc17f26df71645c71a9298
bijay <bijay.subedi@vedastudios.com>
2024-11-12 10:17:55 +0545

    feat: change the value of reacordedAt field

--- src/app/socket/page.tsx
+++ src/app/socket/page.tsx
@@ -125,7 +125,7 @@ function Home() {
           {
             data: `${tempMessage} - ${i}`,
             iv: `ZGVmYXVsdA==`,
-            recordedAt: new Date().toISOString(),
+            recordedAt: Date.now(),
             groupId: tempGroupId,
             messageId: uuidv4(),
           },
